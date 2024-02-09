e7b6698c8784a2526602ff695576c396e346a792
kfxsmt1 <31915738-kfxsmt1@users.noreply.replit.com>
2024-02-09 22:57:12 +0000

    "Flaskblog"

--- main.py
+++ main.py
@@ -2,15 +2,27 @@ from flask import Flask, render_template
 
 app = Flask('app')
 
+posts = [{
+    'author': 'Junsoo',
+    'title': '해윤이와 곧 일년',
+    'content': 'First post content',
+    'date_posted': 'April 20, 2018'
+}, {
+    'author': 'Seung',
+    'title': '정해윤 사랑해',
+    'content': 'Second post content',
+    'date_posted': 'April 21, 2018'
+}]
+
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', posts=posts)  # Arguments = data
 
 
 @app.route('/about')
 def about():
-  return render_template('about.html')
+  return render_template('about.html', title='About')  # Static
 
 
 '''
@@ -21,4 +33,4 @@ __name__ means global variable that python interpreter already made before runni
 '''
 app.run(host='0.0.0.0', port=8080, debug=True)
 
-# https://www.youtube.com/watch?v=QnDWIZuWYW0&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH&index=2  4:41까지 시청
+# https://www.youtube.com/watch?v=QnDWIZuWYW0&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH&index=3 Bootstrap
