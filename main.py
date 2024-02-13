e608b5ba70956ee2c12c6b05f90c8b8567314f7f
Junsoo-cloud <116707341+Junsoo-cloud@users.noreply.github.com>
2024-02-13 14:00:56 +0000

    "Flaskblog"

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
-app = Flask('app')
-
+db = SQLAlchemy()  # database instance
+app = Flask(__name__)
 app.config['SECRET_KEY'] = '34e2ee1548491bcf261d357431cf64e9'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db.init_app(app)
+
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+  password = db.Column(db.String(60), nullable=False)
+  posts = db.relationship('Post', backref='author', lazy=True)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(100), nullable=False)
+  date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+  content = db.Column(
+      db.Text,
+      nullable=False)  # db.Text is unlimited text contrasted to db.String
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+  def __repr__(self):
+    return f"Post('{self.title}', '{self.date_posted}')"
+
+
 posts = [{
     'author': 'Junsoo',
     'title': 'Flaskblog 1',
@@ -56,7 +87,7 @@ __name__ means global variable that python interpreter already made before runni
 '''
 app.run(host='0.0.0.0', port=8080, debug=True)
 
-# https://www.youtube.com/watch?v=QnDWIZuWYW0&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH&index=3 Bootstrap
+# https://www.youtube.com/watch?v=cYWiDiIUxQc&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH&index=4
 # https://www.geeksforgeeks.org/flask-wtf-explained-how-to-use-it/
 
 # https://www.geeksforgeeks.org/get-post-requests-using-python/
