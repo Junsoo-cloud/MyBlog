e1f98c8e61b8759d6763c35cd5da66460d82bf2a
Junsoo-cloud <116707341+Junsoo-cloud@users.noreply.github.com>
2024-02-21 14:57:37 +0000

    "Flaskblog"
    "Flaskblog"

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ if __name__ == '__main__':
 # 1905
 
 # https://www.youtube.com/watch?v=CSHx6eCkmv0&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH&index=6 19분25초
+# https://www.elancer.co.kr/blog/view?seq=162 PHP 관한블로그
