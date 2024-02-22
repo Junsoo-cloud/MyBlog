6750faf75d5f78baa9c65815314212b1888726ed
Junsoo-cloud <116707341+Junsoo-cloud@users.noreply.github.com>
2024-02-22 14:10:38 +0000

    "Flaskblog"
    "Flaskblog"

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms import validators
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
+from flask_login import current_user
 
 
 class RegistrationForm(FlaskForm):
@@ -35,3 +37,25 @@ class LoginForm(FlaskForm):
   password = PasswordField('Password', validators=[DataRequired()])
   remember = BooleanField('Remember Me')
   submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+  username = StringField('Username',
+                         validators=[DataRequired(),
+                                     Length(min=2, max=20)])
+  email = StringField('Email', validators=[DataRequired(), Email()])
+  picture = FileField('Update Profile Picture',
+                      validators=[FileAllowed(['jpg', 'png'])])
+  submit = SubmitField('Update')
+
+  def validate_username(self, username):
+    if username.data != current_user.username:
+      user = User.query.filter_by(username=username.data).first()
+      if user:
+        raise ValidationError('Please choose another Username !')
+
+  def validate_email(self, email):
+    if email.data != current_user.email:
+      user = User.query.filter_by(email=email.data).first()
+      if user:
+        raise ValidationError('Please choose another Email !')
--- main.py
+++ main.py
@@ -5,5 +5,14 @@ if __name__ == '__main__':
 # 827 5
 # 1905
 
-# https://www.youtube.com/watch?v=CSHx6eCkmv0&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH&index=6 19분25초
+# https://www.youtube.com/watch?v=CSHx6eCkmv0&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH&index=7 26분
 # https://www.elancer.co.kr/blog/view?seq=162 PHP 관한블로그
+
+# https://www.geeksforgeeks.org/post-redirect-get-prg-design-pattern/ POST GET pattern
+'''
+This line of code represents an HTML form element.
+
+The method="POST" attribute specifies that the form data will be sent to the server using the POST HTTP method, which is commonly used for submitting data to a server.
+The action="" attribute defines the URL where the form data will be submitted. In this case, the empty string means that the form will be submitted to the current URL.
+The enctype="multipart/form-data" attribute specifies how the form data should be encoded before sending it to the server. It is used when the form includes file upload fields, allowing files to be uploaded along with other form data.
+'''
