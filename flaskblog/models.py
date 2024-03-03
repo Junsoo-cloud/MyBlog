bcefff297737815a09fcd9ce3b5db24ca978dd39
Junsoo-cloud <116707341+Junsoo-cloud@users.noreply.github.com>
2024-03-03 06:22:42 +0000

    "Flaskblog"

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,4 +1,5 @@
-from flaskblog import db, login_manager
+from flaskblog import db, login_manager, app
+from itsdangerous import URLSafeTimedSerializer as Serializer
 from datetime import datetime
 from flask_login import UserMixin
 
@@ -16,6 +17,13 @@ class User(db.Model, UserMixin):
   password = db.Column(db.String(60), nullable=False)
   posts = db.relationship('Post', backref='author', lazy=True)
 
+  def get_reset_token(self, expires_sec=1800):
+    s = Serializer(app.config['SECRET_KEY'], expires_sec)
+    return s.dumps({'user_id': self.id}).decode('utf-8')
+
+  def verify_reset_token(token):
+    s = Serializer(app.config['SECRET_KEY'])
+    
   def __repr__(self):
     return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
